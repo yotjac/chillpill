5e14c995d466c686553b7525d7a01b21735e5820
yotamj <yoyotam.j@gmail.com>
2026-03-14 15:09:10 +0200

    Update versionCode and versionName in build.gradle.kts to prepare for release 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.chillpillapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
